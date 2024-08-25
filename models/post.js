e85a1db361d09d0d09268fee303a6a81ecccdc7f
Stacy <jlhclientsolutions@gmail.com>
2024-08-25 18:12:01 -0400

    added models for posts and comments to posts

---
+++ models/post.js
@@ -0,0 +1,47 @@
+//module.exports = (sequelize, DataTypes) => {
+//    const Post = sequelize.define('Post', {
+//        title: {
+//            type: DataTypes.STRING,
+//            allowNull: false,
+//        },
+//        content: {
+//            type: DataTypes.TEXT,
+//            allowNull: false,
+//        },
+//    });
+//
+//    return Post;
+//};
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Post extends Model {}
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post',
+    }
+);
+
+module.exports = Post;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebarsw');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 
@@ -13,23 +13,24 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {},
-    resave: false,
-    savUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-};
 
-app.use(session(sess));
+
+app.use(session({
+        secret: 'Super secret secret',
+        cookie: {},
+        resave: true,
+        savUninitialized: false,
+        store: new SequelizeStore({
+            db: sequelize
+        })
+    }
+));
 
 app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handebars');
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extened: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
